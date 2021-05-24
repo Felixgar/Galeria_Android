77ad0d9d44748f9c943e4e9037d33037307063b9
FlutLab.io <>
2021-05-24 23:22:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MiagendaApp());
+void main() => runApp(Miagenda());
 
-class MiagendaApp extends StatelessWidget {
+class Miagenda extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(debugShowCheckedModeBanner: false, title: "galeria fotografica", home: new PaginaPrincipal(title: "Galeria de imagenes de benitez")); //fin de materialapp return
-  } //fin widget miagendaapp
-} //fin clase miagendaapp
+    return MaterialApp(
+      title: 'Galeria de fotos',
+      home: PaginaPrincipal(title: 'GridView Galeria de fotos'),
+    );
+  }
+}
 
 class PaginaPrincipal extends StatefulWidget {
   final String title;
@@ -16,36 +19,34 @@ class PaginaPrincipal extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new PaginaPrincipalState();
-  }
-} //fin clase pagina principal con estado
+  } //
+} //fin de pagina principal
 
 class PaginaPrincipalState extends State<PaginaPrincipal> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
-      ), //fin del appbar
+      ),
       body: new GridView.extent(
         maxCrossAxisExtent: 150.0,
         mainAxisSpacing: 5.0,
         crossAxisSpacing: 5.0,
         padding: const EdgeInsets.all(5.0),
-        //29 porque son 29 imagenes las que vamos a subir editamos ahi para los numeros a subir de imagenes
         children: _buildGridTiles(29),
-      ), //fin del gridview
-    ); //fin de scaffold
-  } //fin del widget
-} //fin de la clase paginaprincipalstate
+      ), //fin de gridview
+    ); //fin de sacaffold
+  } //fin de widget build
+} //fin de class Pagina principal
 
 List<Widget> _buildGridTiles(numberOfTiles) {
   List<Container> containers = new List<Container>.generate(numberOfTiles, (int index) {
     //index = 0, 1, 2,...
-    final imageName = index < 9 ? 'images/image0${index + 1}.JPG' : 'images/image${index + 1}.JPG';
+    final imageName = index < 9 ? 'images/imagen0${index + 1}.jpg' : 'images/imagen${index + 1}.jpg';
     return new Container(
       child: new Image.asset(imageName, fit: BoxFit.fill),
     );
-  } //fin index
-      ); //fin del generador
+  });
   return containers;
-} //fin de list de widget para las fotos
+}
